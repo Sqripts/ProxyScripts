4f44a11e708751901b190e28dfc122eb7eb66225
Oliver Muthomi <oliver.mwitia@gmail.com>
2019-06-18 12:21:31 +0300

    Script to set up global menus in kde plasma

---
+++ plasma_global_menu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Error Title: Kde global menu for GTK Apps
+# Solution: Install appmenu-gtk-module
+
+sudo pacman -S appmenu-gtk-module
+sudo pacman -S libdbusmenu-glib
+sudo pacman -S libdbusmenu-gtk3
+sudo pacman -S libdbusmenu-gtk2
+sudo pacman -S libdbusmenu-qt5
